c06f1a836f0a3b95f77056782258c08a32d25d2a
Wasdns <952693358@qq.com>
2017-03-23 18:30:15 +0800

    Update Demo
    Update Demo

--- Demo/add_entry.sh
+++ Demo/add_entry.sh
@@ -13,3 +13,9 @@ sudo ./simple_switch_CLI --thrift-port 22222 < commands.txt
 sudo ./simple_switch_CLI --thrift-port 22223 < commands.txt
 sudo ./simple_switch_CLI --thrift-port 22224 < commands.txt
 sudo ./simple_switch_CLI --thrift-port 22225 < commands.txt
+
+# oldcmds:
+# sudo ./simple_switch_CLI --thrift-port 22222 < commands_s1.txt
+# sudo ./simple_switch_CLI --thrift-port 22223 < commands_s2.txt
+# sudo ./simple_switch_CLI --thrift-port 22224 < commands_s3.txt
+
